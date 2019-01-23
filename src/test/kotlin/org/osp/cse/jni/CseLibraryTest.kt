413d532cae769d47f7b259843fac2a9398b9209e
Lars Ivar Hatledal <laht@ntnu.no>
2019-01-23 16:34:40 +0100

    add new test

--- src/test/kotlin/org/osp/cse/jni/CseLibraryTest.kt
+++ src/test/kotlin/org/osp/cse/jni/CseLibraryTest.kt
@@ -6,14 +6,35 @@ import java.time.Duration
 
 class CseLibraryTest {
 
+    companion object {
+        val test_fmu = CseLibraryTest::class.java.classLoader
+            .getResource("fmus/2.0/cs/20sim/4.6.4.8004/ControlledTemperature/ControlledTemperature.fmu").file
+    }
 
     @Test
-    fun testLibrary() {
+    fun testLibrary1() {
 
         CseLibrary().apply {
-
             val execution = createExecution(0, Duration.ofSeconds(1/100).toNanos())
             Assertions.assertTrue(destroyExecution(execution))
+
+        }
+
+    }
+
+    @Test
+    fun testLibrary2() {
+
+        CseLibrary().apply {
+            val execution = createExecution(0, Duration.ofSeconds(1/100).toNanos())
+
+            val slave = createLocalSlave(test_fmu)
+            val index = addSlave(execution, slave)
+
+            Assertions.assertTrue(step(execution, 1))
+
+            Assertions.assertTrue(destroyExecution(execution))
+
         }
 
     }
