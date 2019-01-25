062cb3a013db65eec1273808ec8576e13f9f9c93
Lars Ivar Hatledal <laht@ntnu.no>
2019-01-25 10:37:44 +0100

    Add CseSamples

---
+++ src/main/kotlin/org/osp/cse/CseSamples.kt
@@ -0,0 +1,37 @@
+package org.osp.cse
+
+import java.util.*
+
+
+sealed class CseSamples{
+
+    val size: Int = -1
+
+    lateinit var steps: LongArray
+        private set
+
+    lateinit var times: DoubleArray
+        private set
+
+}
+
+class CseRealSamples: CseSamples() {
+
+    lateinit var values: DoubleArray
+        private set
+
+    override fun toString(): String {
+        return "CseRealSamples(size=$size\nvalues=${Arrays.toString(values)}\nsteps=${Arrays.toString(steps)}\ntimes=${Arrays.toString(times)})"
+    }
+
+}
+class CseIntegerSamples: CseSamples() {
+
+    lateinit var values: IntArray
+        private set
+
+    override fun toString(): String {
+        return "CseIntegerSamples(size=$size\nvalues=${Arrays.toString(values)}\nsteps=${Arrays.toString(steps)}\ntimes=${Arrays.toString(times)})"
+    }
+
+}
