44ce5b0573c9302e75483fa40c3180396dbf6787
Lars Ivar Hatledal <laht@ntnu.no>
2019-06-26 09:30:41 +0200

    setup cmake project for JNI wrapper

---
+++ csecorejni/cmake-build-release/src/CMakeFiles/csecorejni.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "csecorejni.lib"
+)
---
+++ csecorejni/conanfile.py
@@ -0,0 +1,20 @@
+
+from conans import ConanFile, CMake
+
+
+class CsecorejniConan(ConanFile):
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    requires = (
+        "cse-core/0.3.0@osp/master"
+    )
+
+    default_options = {
+        "csecore:fmuproxy": True
+    }
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.pdb", dst="bin", keep_path=False)
