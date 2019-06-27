15d0e6f1b882df53d7b11bee8310a87f015c2590
Lars Ivar Hatledal <laht@ntnu.no>
2019-06-27 14:54:49 +0200

    Prepare debug also

--- csecorejni/run_conan_install.sh
+++ csecorejni/run_conan_install.sh
@@ -5,9 +5,8 @@ conan user -p "Open Simulation Platform" -r osp osp
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release --build=missing;;
-    MINGW*)     conan install . -s build_type=Release --install-folder=cmake-build-release --build=missing;;
+    Linux*)     conan install . -s build_type=Release -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-release --build=missing
+                conan install . -s build_type=Debug -s compiler.libcxx=libstdc++11 --install-folder=cmake-build-debug --build=missing;;
+    MINGW*)     conan install . -s build_type=Debug --install-folder=cmake-build-debug --build=missing
+                conan install . -s build_type=Release --install-folder=cmake-build-release --build=missing;;
 esac
-
-
-
