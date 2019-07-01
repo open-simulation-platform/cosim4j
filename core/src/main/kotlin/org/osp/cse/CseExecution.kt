d505c3c7f16a2cfb8b03c83f64c333d9d64dc0f1
Lars Ivar Hatledal <laht@ntnu.no>
2019-07-01 07:00:39 +0200

    Move class

--- core/src/main/kotlin/org/osp/cse/CseExecution.kt
+++ core/src/main/kotlin/org/osp/cse/CseExecution.kt
@@ -154,3 +154,32 @@ class CseExecution private constructor(
     }
 
 }
+
+class CseExecutionStatus {
+
+    var currentTime: Double = -1.0
+        private set
+
+    var realTimeFactor: Double = -1.0
+        private set
+
+    var realTimeSimulation: Boolean = false
+        private set
+
+    private var stateId: Int = -1
+
+    val state: CseExecutionState by lazy {
+        CseExecutionState.valueOf(stateId)
+    }
+
+    private var errorCodeId: Int = -1
+
+    val errorCode: CseErrorCode by lazy {
+        CseErrorCode.valueOf(errorCodeId)
+    }
+
+    override fun toString(): String {
+        return "CseExecutionStatus(currentTime=$currentTime, state=$state, errorCode=$errorCode, realTimeFactor=$realTimeFactor, isRealTimeSimulation=$realTimeSimulation)"
+    }
+
+}
