ceb1a1ba430f925c2e8e85f33c96dc152a30586a
Lars Ivar Hatledal <laht@ntnu.no>
2019-11-09 10:05:42 +0100

    Update conan script

--- cpp/cse-core4j/run_conan_install.sh
+++ cpp/cse-core4j/run_conan_install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-export CONAN_REVISIONS_ENABLED="1"
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan --f
-conan remote add osp https://osp-conan.azurewebsites.net/artifactory/api/conan/public --f
+conan remote add osp https://osp-conan.azurewebsites.net/artifactory/api/conan/local --f
 conan user -p "Open Simulation Platform" -r osp osp
 
 unameOut="$(uname -s)"
