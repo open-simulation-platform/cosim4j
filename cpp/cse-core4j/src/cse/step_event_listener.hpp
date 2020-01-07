dc83c8a98ec58c32d6da8137fe75d5b05ac99e62
Lars Ivar Hatledal <laht@ntnu.no>
2020-01-07 11:18:28 +0100

    Update cse

--- cpp/cse-core4j/src/cse/step_event_listener.hpp
+++ cpp/cse-core4j/src/cse/step_event_listener.hpp
@@ -17,6 +17,7 @@ class step_event_listener : public observer
 public:
     step_event_listener(JNIEnv* env, jobject listener);
 
+    void simulation_initialized(step_number firstStep, time_point startTime) override;
     void simulator_added(simulator_index index, observable* observable, time_point point) override;
     void simulator_removed(simulator_index index, time_point point) override;
     void variables_connected(variable_id output, variable_id input, time_point point) override;
@@ -34,20 +35,22 @@ private:
     thread_worker worker_;
 };
 
-
-inline void step_event_listener::simulator_added(simulator_index index, observable* observable, time_point point)
+inline void step_event_listener::simulation_initialized(step_number, time_point)
 {
 }
-inline void step_event_listener::simulator_removed(simulator_index index, time_point point)
+inline void step_event_listener::simulator_added(simulator_index, observable*, time_point)
 {
 }
-inline void step_event_listener::variables_connected(variable_id output, variable_id input, time_point point)
+inline void step_event_listener::simulator_removed(simulator_index, time_point)
 {
 }
-inline void step_event_listener::variable_disconnected(variable_id input, time_point point)
+inline void step_event_listener::variables_connected(variable_id, variable_id, time_point)
 {
 }
-inline void step_event_listener::simulator_step_complete(simulator_index index, step_number lastStep, duration lastStepSize, time_point currentTime)
+inline void step_event_listener::variable_disconnected(variable_id, time_point)
+{
+}
+inline void step_event_listener::simulator_step_complete(simulator_index, step_number, duration, time_point)
 {
 }
 
