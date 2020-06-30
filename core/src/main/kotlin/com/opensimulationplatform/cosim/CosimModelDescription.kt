bb2910a280ee57095b0b90e1fa5ec0bd26c9c7f1
Lars Ivar Hatledal <laht@hials.no>
2020-06-30 08:45:09 +0200

    Add XXXorNull functions

--- core/src/main/kotlin/com/opensimulationplatform/cosim/CosimModelDescription.kt
+++ core/src/main/kotlin/com/opensimulationplatform/cosim/CosimModelDescription.kt
@@ -10,13 +10,21 @@ class CosimModelDescription(
 ) {
 
     fun getVariable(variableName: String): CosimVariableDescription {
+        return getVariableOrNull(variableName)
+            ?: throw NoSuchElementException("No variable with name='$variableName' found in model '$modelName'!")
+    }
+
+    fun getVariableOrNull(variableName: String): CosimVariableDescription? {
         return variables.find { it.name == variableName }
-            ?: throw IllegalArgumentException("No variable with name='$variableName' found in model '$modelName'!")
     }
 
     fun getVariable(vr: Long, type: CosimVariableType): CosimVariableDescription {
+        return getVariableOrNull(vr, type)
+            ?: throw NoSuchElementException("No variable with vr=$vr of type=$type found in model '$modelName'!")
+    }
+
+    fun getVariableOrNull(vr: Long, type: CosimVariableType): CosimVariableDescription? {
         return variables.find { it.valueReference == vr && it.type == type }
-            ?: throw IllegalArgumentException("No variable with vr=$vr of type=$type found in model '$modelName'!")
     }
 
     override fun toString(): String {
